ab0e641ccb534e41d1eb66175675e9bd2d4b198d
ayayaneru <870672449@gmail.com>
2020-10-17 14:59:10 +0800

    day07done

--- www/handlers.py
+++ www/handlers.py
@@ -4,3 +4,17 @@
 __author__ = 'Michael Liao'
 
 ' url handlers '
+
+import re, time, json, logging, hashlib, base64, asyncio
+
+from coroweb import get, post
+
+from models import User, Comment, Blog, next_id
+
+@get('/')
+async def index(request):
+    users = await User.findAll()
+    return {
+        '__template__': 'test.html',
+        'users': users
+    }
