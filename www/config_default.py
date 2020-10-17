5fa57b94c5fb92a59172bcd0d01b49246dafb8ce
ayayaneru <870672449@gmail.com>
2020-10-17 09:42:20 +0800

    day06done

---
+++ www/config_default.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Default configurations.
+'''
+
+__author__ = 'Ayayaneru'
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': '369874125',
+        'db': 'moe'
+    },
+    'session': {
+        'secret': 'moe'
+    }
+}
